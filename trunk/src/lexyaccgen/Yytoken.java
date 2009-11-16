780c6288c0e1ddf6226b87fe76385644b74a2d44
mihahh <mihahh@8fa50396-be56-11de-8784-f7ad4773dbab>
2009-11-16 15:38:03 +0000

    yytoken

--- trunk/src/lexyaccgen/Yytoken.java
+++ trunk/src/lexyaccgen/Yytoken.java
@@ -3,27 +3,31 @@ package lexyaccgen;
 public class Yytoken {
 
 	private int		m_token;
-	private String	m_lexem;	
+	private String	m_lexem;
+	private int		m_length;
+	
 	
 	public Yytoken(int token, String lexem) {
 			
 		m_token = token;
 		m_lexem = new String(lexem);
+		m_length = lexem.length();
 	}
 	
-	public int getTokenType() {
-		
+	public int getTokenType() {		
 		return m_token;
 	}
 	
-	public String getLexem() {
-		
+	public String getLexem() {		
 		return m_lexem;
 	}
 	
+	public int length() {
+		return m_length;
+	}
+	
 	@Override
-	public String toString() {
-		
+	public String toString() {		
 		return (new String(m_token + ": " + m_lexem));
 	}
 
