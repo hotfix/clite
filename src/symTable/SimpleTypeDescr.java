fea41d8c942e7f0dd542b2a0c6979f80c31be332
alexander.albrant <alexander.albrant@8fa50396-be56-11de-8784-f7ad4773dbab>
2010-01-10 17:34:20 +0000

    SimpleTypeDescr

---
+++ src/symTable/SimpleTypeDescr.java
@@ -0,0 +1,36 @@
+package symTable;
+
+public class SimpleTypeDescr extends AbstractDescr {
+	
+	
+
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private int size;
+	
+	
+	public SimpleTypeDescr(){
+		name = "";
+		size = 0;		
+	}
+	
+	public SimpleTypeDescr(String name, int size){
+		this.name = name;
+		this.size = size;
+		
+	}
+	
+	public String GetName() {	
+		return name;
+	}
+	
+	public int GetSize() {
+		return size;
+	}
+	
+	@Override
+	public String toString() {
+		return "SimpleTypeDescr [name=" + name + ", size=" + size + "]";
+	}
+}
