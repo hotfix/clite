bdd073ee0cf3b449d6c22234f95f7d0c00eee5c5
alexander.albrant <alexander.albrant@8fa50396-be56-11de-8784-f7ad4773dbab>
2010-01-12 15:42:30 +0000

    implemented

--- trunk/src/abstractTreeNodes/ContNode.java
+++ trunk/src/abstractTreeNodes/ContNode.java
@@ -1,10 +1,15 @@
 package abstractTreeNodes;
 
+import symTable.VarEntry;
+import instructions.ContInstr;
+import instructions.IntVal;
+import codeGen.CodeGen;
+
 public class ContNode extends AbstractNode {
 
 	private static final long serialVersionUID = 1L;
-	private AbstractNode child;
 	
+	private AbstractNode child;	
 	public ContNode(AbstractNode child) {
 		
 		this.child = child;
@@ -24,6 +29,10 @@ public class ContNode extends AbstractNode {
 	
 	@Override
 	public void Compile() {
+		System.out.println("ContNode::Compile");
+		
+		child.Compile();
+		CodeGen.OutInstr(new ContInstr());
 
 	}
 }
